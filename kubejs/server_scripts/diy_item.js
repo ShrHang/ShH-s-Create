88744a182b69b5885d7ee094c7a8ddc5e9bb0f07
ShrHang <shhshrhang@gmail.com>
2025-09-03 12:09:41 +0800

    9-2-update-1

---
+++ kubejs/server_scripts/diy_item.js
@@ -0,0 +1,55 @@
+ServerEvents.recipes(event => {
+    event.shaped('shh:maid_tool', [
+        '  A',
+        ' B ',
+        'B  '
+    ], {
+        A: 'l2complements:heirophant_green',
+        B: 'create_sa:zinc_handle'
+    })
+
+    event.custom(
+        {
+            type: "create:mixing",
+            ingredients: [
+                {
+                    item: "minecraft:egg"
+                },
+                {
+                    item: "create:wheat_flour"
+                },
+                {
+                    item: "create:cinder_flour"
+                },
+                {
+                    type: "fluid_stack",
+                    amount: 1000,
+                    fluid: "minecraft:milk"
+                },
+                {
+                    item: "minecraft:sugar"
+                },
+                {
+                    item: "minecraft:sugar"
+                },
+                {
+                    item: "minecraft:sugar"
+                },
+                {
+                    item: "minecraft:sugar"
+                },
+                {
+                    item: "minecraft:sugar"
+                },
+                {
+                    item: "minecraft:sugar"
+                }
+            ],
+            results: [
+                {
+                    id: "shh:hulibugulv"
+                }
+            ]
+        }
+    )
+})
